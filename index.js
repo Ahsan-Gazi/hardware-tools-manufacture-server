f04cb6472fc94744e2fe6196d96043bf4adb3c59
Ahsan-Gazi <ahsangazi01@gmail.com>
2022-05-25 23:29:52 +0600

    server connected

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+ //const ObjectId = require('mongodb').ObjectId;
+
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.snip6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsmanufacture = client.db('tools-manufacture').collection('tools');
+        console.log('toolsmanufacture');
+
+      
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hardware tools manufacture!');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
